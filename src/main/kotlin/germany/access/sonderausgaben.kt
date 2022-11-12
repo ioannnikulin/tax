8778c3482a2a680e050c1fdb642e6a5547dabfd8
ioann <no@no.no>
2022-11-12 13:00:47 +0100

    added several sonderausgaben (not precise, just declared some types)

--- src/main/kotlin/germany/access/sonderausgaben.kt
+++ src/main/kotlin/germany/access/sonderausgaben.kt
@@ -3,13 +3,73 @@ package tax.germany.access
 import tax.LegalReduction
 import tax.Reduction
 import java.io.File
+import java.lang.Double.max
 
 interface Sonderausgaben: LegalReduction {
 }
 
-class StudyExpenses(var amount:Double, var proof: File? = null): Reduction(amount, proof), Sonderausgaben {
+class StudyExpenses(amount:Double, proof: File? = null): Reduction(amount, proof), Sonderausgaben {
+    var amount = amount
+        get() = //TODO: one thing is a sum that the subject wants to deduct, and the other thing is the sum that will actually be deducted,
+// and the latter can be combined from multiple priors
 }
 
-class PostMaritalMaintenance(var amount:Double, var proof: File? = null): Reduction(amount, proof), Sonderausgaben {
-     //TODO:
+class PostMaritalMaintenance(amount:Double, proof: File? = null)://Unterhalt
+    Reduction(amount, proof), Sonderausgaben {
+    var amount = amount
+        get() = max(field, 13805.0)//TODO1: for some reason only spouse stated, need to check more
+    // https://de.wikipedia.org/wiki/Sonderausgabe_(Steuerrecht)
+    override val desc: String
+        get() = "If you have been paying maintenance to a divorced partner after submitting joint tax declaration for some time, " +
+                "your taxable income can be reduced by that maintenance. " +
+                "For further details see https://de.wikipedia.org/wiki/Unterhalt_(Deutschland) or go to court"
+}
+
+open class InsuranceContribution(amount: Double, proof: File? = null):
+    Reduction(amount, proof), Sonderausgaben {
+        //TODO1: actually all insurance contributions in sum shouldn't be over 2800 or 1900, didn't understand
+        //so children should call insurancecontribution standalone (one per person) and change his amount
+        //also some even more complex prerequisites under (2) in estg 10, jesus christ
+        //also some fuckery in 10a
+    }
+
+class OldAgePensionContribution(amount:Double, proof: File? = null):
+    InsuranceContribution(amount, proof) {
+}
+
+class OccupationalDisabilityInsuranceContribution(amount:Double, proof: File? = null):
+    InsuranceContribution(amount, proof) {
+}
+
+class UnemploymentInsuranceContribution(amount:Double, proof: File? = null):
+    InsuranceContribution(amount, proof) {
+}
+
+class HealthInsuranceContribution(amount:Double, proof: File? = null):
+    InsuranceContribution(amount, proof) {
+}
+
+class ChurchTaxContribution(amount:Double, proof: File? = null):
+    Reduction(amount, proof), Sonderausgaben {//TODO1: some exception i didnt understand
+}
+
+class ProfessionalTraining(amount:Double, proof: File? = null):
+    Reduction(amount, proof), Sonderausgaben {//TODO1: also something about spouses
+        var amount = amount
+            get() = max(field, 6000.0)//TODO1: also possibly it applies differently to first job training and others...
+}
+
+class ChildSchoolPayment(amount:Double, proof: File? = null):
+    Reduction(amount, proof), Sonderausgaben {
+    var amount = amount
+        get() = max(field/3, 5000.0)
+    override val desc = "Payment for school for each of your children. " +
+            "Only attending lessons - not accommodation, not food, not care, just lessons price. " +
+            "The school mut be within EU or be legally German - anywhere."
+}
+
+class Donations(amount:Double, proof: File? = null):
+    Reduction(amount, proof), Sonderausgaben {
+    var amount = amount
+        get() = field//TODO1: complex limits somehow tied to total brutto and other stuff, have to go up the stairs...
 }
